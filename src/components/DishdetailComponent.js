4b862f992518493a6df90cd15a0ca7ea6cc30df8
Alapan Sau <alapansau@gmail.com>
2020-07-09 06:06:53 +0530

    redux thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
 import {Breadcrumb, BreadcrumbItem, Modal, ModalBody, ModalHeader, Label,  Row, Col} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 function DateFormat(date_var){
@@ -58,7 +59,25 @@ const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 
 function Dishdetail(props){
-
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
         return (
             <div className="container">
                 <div className="row">
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,7 +1,18 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionType from "./ActionTypes";
 
-export const Dishes = (state=DISHES,action)=>{
+
+export const Dishes = (state={
+    isLoading : true,
+    errMess:null,
+    dishes : []    
+    },action)=>{
     switch(action.type){
+        case ActionType.DISH_LOADING:
+            return {...state, isLoading: true, errMess: null, dishes: []}
+        case ActionType.ADD_DISH:
+            return {...state, isLoading: false, errMess: null, dishes: action.payload}
+        case ActionType.DISH_FAILED:
+            return {...state, isLoading: false, errMess: action.payload}
         default :
             return state;
     }
