2da127310a90bfbe179e36c5bbc80827f701db1d
Alapan Sau <alapansau@gmail.com>
2020-07-12 09:37:27 +0530

    assignment 4

--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,7 +1,18 @@
-import { LEADERS } from '../shared/leaders';
+import * as ActionType from "./ActionTypes";
 
-export const Leaders = (state=LEADERS,action)=>{
+
+export const Leaders = (state={
+    isLoading : true,
+    errMess:null,
+    leaders : []    
+    },action)=>{
     switch(action.type){
+        case ActionType.LEADER_LOADING:
+            return {...state, isLoading: true, errMess: null, leaders: []}
+        case ActionType.ADD_LEADER:
+            return {...state, isLoading: false, errMess: null, leaders: action.payload}
+        case ActionType.LEADER_FAILED:
+            return {...state, isLoading: false, errMess: action.payload}
         default :
             return state;
     }
